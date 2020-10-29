4fed10b6513f86a1a5b103f13d9928ca2af31142
Jeffrey Klardie <j.klardie@amsterdam.nl>
2020-10-29 15:11:09 +0100

    Format src using black code formatter

--- src/django_audit_log/logger.py
+++ src/django_audit_log/logger.py
@@ -1,14 +1,13 @@
 import logging
 
+from audit_log.logger import AuditLogger
 from django.http import HttpRequest, HttpResponse
 
-from audit_log.logger import AuditLogger
 from django_audit_log import app_settings
 from django_audit_log.util import get_client_ip, import_callable
 
 
 class DjangoAuditLogger(AuditLogger):
-
     def get_logger_name(self) -> str:
         logger_name = app_settings.LOGGER_NAME
         if not logger_name:
@@ -31,33 +30,39 @@ class DjangoAuditLogger(AuditLogger):
         log_formatter_callable = import_callable(log_formatter_path)
         return log_formatter_callable()
 
-    def set_django_http_request(self, request: HttpRequest) -> 'DjangoAuditLogger':
+    def set_django_http_request(self, request: HttpRequest) -> "DjangoAuditLogger":
         self.set_http_request(
             method=request.method,
             url=request.build_absolute_uri(),
-            user_agent=request.META.get('HTTP_USER_AGENT', '?') if request.META else '?'
+            user_agent=request.META.get("HTTP_USER_AGENT", "?")
+            if request.META
+            else "?",
         )
         return self
 
-    def set_django_http_response(self, response: HttpResponse) -> 'DjangoAuditLogger':
+    def set_django_http_response(self, response: HttpResponse) -> "DjangoAuditLogger":
         headers = self._get_headers_from_response(response)
         self.set_http_response(
-            status_code=getattr(response, 'status_code', ''),
-            reason=getattr(response, 'reason_phrase', ''),
-            headers=headers
+            status_code=getattr(response, "status_code", ""),
+            reason=getattr(response, "reason_phrase", ""),
+            headers=headers,
         )
         return self
 
-    def set_user_from_request(self, request: HttpRequest, realm='') -> 'DjangoAuditLogger':
-        user = request.user if hasattr(request, 'user') else None
-        roles = list(user.groups.values_list('name', flat=True)) if user else []
+    def set_user_from_request(
+        self, request: HttpRequest, realm=""
+    ) -> "DjangoAuditLogger":
+        user = request.user if hasattr(request, "user") else None
+        roles = list(user.groups.values_list("name", flat=True)) if user else []
         self.set_user(
             authenticated=user.is_authenticated if user else False,
-            provider=request.session.get('_auth_user_backend', '') if hasattr(request, 'session') else '',
+            provider=request.session.get("_auth_user_backend", "")
+            if hasattr(request, "session")
+            else "",
             realm=realm,
-            email=getattr(user, 'email', '') if user else '',
+            email=getattr(user, "email", "") if user else "",
             roles=roles,
-            ip=get_client_ip(request)
+            ip=get_client_ip(request),
         )
         return self
 
--- src/django_audit_log/middleware.py
+++ src/django_audit_log/middleware.py
@@ -8,20 +8,21 @@ from django_audit_log.logger import DjangoAuditLogger
 
 
 class AuditLogMiddleware(MiddlewareMixin):
-
     def __init__(self, get_response=None):
         super().__init__(get_response)
         self.redirect_exempt = [re.compile(r) for r in app_settings.EXEMPT_URLS]
 
     def process_request(self, request: HttpRequest) -> None:
-        if not hasattr(request, 'audit_log') and not self.exempt_request(request):
+        if not hasattr(request, "audit_log") and not self.exempt_request(request):
             audit_log = DjangoAuditLogger()
             audit_log.set_django_http_request(request)
             audit_log.set_user_from_request(request)
             request.audit_log = audit_log
 
-    def process_response(self, request: HttpRequest, response: HttpResponse) -> HttpResponse:
-        if hasattr(request, 'audit_log'):
+    def process_response(
+        self, request: HttpRequest, response: HttpResponse
+    ) -> HttpResponse:
+        if hasattr(request, "audit_log"):
             audit_log = request.audit_log
             audit_log.set_django_http_response(response)
             audit_log.send_log()
